6207ec2502a5aeffd70cbc5c925528e2ad2eceed
Wisdom Banso <bansowisdom@gmail.com>
2021-06-25 12:54:00 +0100

    feat(*): Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
-import { Component } from "@angular/core";
-import { environment } from "./../environments/environment";
+import { Component, Inject } from "@angular/core";
+import { ProductService } from "./shared/services/product.service";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  styleUrls: ["./app.component.css"],
+  providers: [],
 })
 export class AppComponent {
-  API_URL;
-  constructor() {
-    this.API_URL = environment.apiURL;
+  constructor(@Inject(ProductService) private productService: ProductService) {
+    this.productService.getFirstFiveProductsInStore().subscribe(
+      res => console.log(res[0]),
+    );
   }
 }
---
+++ src/app/shared/services/product.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from "@angular/common/http";
+import { Inject, Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { environment } from "../../../environments/environment";
+
+@Injectable({ providedIn: 'root' })
+export class ProductService {
+  private readonly apiUrl = `https://cors-anywhere.herokuapp.com/${environment.apiURL}`;
+
+  constructor(@Inject(HttpClient) private readonly http: HttpClient) {}
+
+  public getFirstFiveProductsInStore(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
+}
